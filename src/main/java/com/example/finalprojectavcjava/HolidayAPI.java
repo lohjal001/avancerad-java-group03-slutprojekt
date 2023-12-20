b9368972a5613b62b9233465ccc040bcac0c3b12
lohjal001 <113910139+lohjal001@users.noreply.github.com>
2023-12-20 08:10:23 +0100

    API

---
+++ src/main/java/com/example/finalprojectavcjava/HolidayAPI.java
@@ -0,0 +1,69 @@
+package com.example.finalprojectavcjava;
+
+import cn.hutool.json.JSONObject;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonArray;
+import com.eclipsesource.json.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class HolidayAPI {
+    private static String API_KEY = "713f3086-0827-4f03-b2e3-a41e985b12c1";
+
+    private static String API_URL = "https://holidayapi.com/v1/holidays?pretty&key=713f3086-0827-4f03-b2e3-a41e985b12c1&country=SE&year=2022";
+
+    public static void Holidays(String[] args) {
+        JSONObject obj = new JSONObject();
+
+
+        try {
+            URL url = new URL(String.format(API_URL, API_KEY));
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            StringBuilder response = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            JsonObject holidaysOne = Json.parse(response.toString()).asObject();
+
+
+
+            JsonArray holidaysArray = holidaysOne.get("holidays").asArray();
+            System.out.println(holidaysArray);
+
+            JsonObject holidaysString = holidaysArray.get(0).asObject();
+            System.out.println(holidaysString);
+
+            String holidayName = holidaysString.getString("name", "missing");
+            System.out.println(holidayName);
+
+            String holidayDate= holidaysString.getString("date", "missing");
+            System.out.println(holidayDate);
+
+
+
+
+            reader.close();
+            connection.disconnect();
+
+
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
